14275a784bd17535b2dc0a51b3ca15a30f49af29
monalisazyx <madhurimazyx@gmail.com>
2023-10-19 08:32:17 +0530

    Add color and rainbow features

--- project.js
+++ project.js
@@ -1,11 +1,27 @@
 let container = document.querySelector('#container');
 let btn = document.querySelector('button');
+let color = document.querySelector('#color');
+let rainbow = document.querySelector('#rainbow');
+
+function getColor() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    return `rgb(${r}, ${g}, ${b})`
+
+}
 btn.addEventListener('click', () => {
  while(container.firstChild){
     container.removeChild(container.firstChild);
  }
  let x = Number(prompt('num: '));
+ while(x > 100){
+    x = Number(prompt('num: '));
+
+
+ }
  let cellSize = 600/x;
+ 
 
  for(let j = 0; j < x ; j++){  //loop for the rows
     let row = document.createElement('div');    //create a row
@@ -27,15 +43,108 @@ btn.addEventListener('click', () => {
 //let drawing = false;
  col.forEach((column) => {
     column.addEventListener('mouseenter' , () => {
-        column.style.backgroundColor = '#9068be';
+        column.style.backgroundColor = getColor();
 
     });
    
     column.addEventListener('mouseup' , () => {
-        column.style.backgroundColor = '#9068be';
+        column.style.backgroundColor = getColor();
 
     });
     
  });
 
 });
+color.addEventListener('click', () => {
+
+    while(container.firstChild){
+       container.removeChild(container.firstChild);
+    }
+    let x = Number(prompt('num: '));
+    while(x > 100){
+       x = Number(prompt('num: '));
+   
+   
+    }
+    let cellSize = 600/x;
+    
+   
+    for(let j = 0; j < x ; j++){  //loop for the rows
+       let row = document.createElement('div');    //create a row
+       row.classList.add('row');
+       for(let i = 0; i < x ; i++ ){   //loop for the columns
+           let column = document.createElement('div');
+           column.classList.add('column');
+           column.style.width = cellSize + 'px';
+           column.style.height = cellSize + 'px';
+   
+           row.appendChild(column);   //to append the cells to each row 
+           
+       }
+       container.appendChild(row);
+   
+   }
+   
+    const col = document.querySelectorAll('.column');
+   //let drawing = false;
+   let select = prompt("Enter desired color: ");
+    col.forEach((column) => {
+       column.addEventListener('mouseenter' , () => {
+           column.style.backgroundColor = select;
+   
+       });
+      
+       column.addEventListener('mouseup' , () => {
+           column.style.backgroundColor = select;
+   
+       });
+       
+    });
+   
+});
+rainbow.addEventListener('click', () => {
+
+    while(container.firstChild){
+       container.removeChild(container.firstChild);
+    }
+    let x = Number(prompt('num: '));
+    while(x > 100){
+       x = Number(prompt('num: '));
+   
+   
+    }
+    let cellSize = 600/x;
+    
+   
+    for(let j = 0; j < x ; j++){  //loop for the rows
+       let row = document.createElement('div');    //create a row
+       row.classList.add('row');
+       for(let i = 0; i < x ; i++ ){   //loop for the columns
+           let column = document.createElement('div');
+           column.classList.add('column');
+           column.style.width = cellSize + 'px';
+           column.style.height = cellSize + 'px';
+   
+           row.appendChild(column);   //to append the cells to each row 
+           
+       }
+       container.appendChild(row);
+   
+   }
+   
+    const col = document.querySelectorAll('.column');
+   //let drawing = false;
+    col.forEach((column) => {
+       column.addEventListener('mouseenter' , () => {
+           column.style.backgroundColor = getColor();
+   
+       });
+      
+       column.addEventListener('mouseup' , () => {
+           column.style.backgroundColor = getColor();
+   
+       });
+       
+    });
+   
+}); 
